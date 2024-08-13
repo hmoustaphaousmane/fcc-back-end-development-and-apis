fe9a4fdcf3d1c360ada004fb860f73a1cd265a4d
Hassane Moustapha Ousmane <hmoustaphaousmane01@gmail.com>
2024-08-13 14:14:49 +0000

    feat: set up /api/whoami endpoint

--- boilerplate-project-headerparser/index.js
+++ boilerplate-project-headerparser/index.js
@@ -19,6 +19,20 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/whoami', function (req, res) {
+  // get ip adress
+  let ipAddress = req.ip;
+
+  // get local language
+  let language = req.headers['accept-language'];
+
+  // get softwer inforamtions
+  let software = req.headers['user-agent'];
+
+  res.json({ ipaddress:  ipAddress, language: language, software: software });
+})
+
+
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
